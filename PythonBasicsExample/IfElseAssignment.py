104f2ab89242b4853243510fc49a388552b8592d
khasakinil <khasakinil@gmail.com>
2023-10-22 16:59:47 +0530

    If Else Assignement

---
+++ PythonBasicsExample/IfElseAssignment.py
@@ -0,0 +1,15 @@
+
+grade = 75
+
+if grade >=0 and grade <=59:
+    print('F')
+elif grade >=60 and grade <=69:
+    print('D')
+elif grade >=70 and grade <=79:
+    print('C')
+elif grade >=80 and grade <=89:
+    print('B')
+elif grade >=90 and grade <=100:
+    print('A')
+else :
+    print("Invalid grades")
