b700efce8a988185ab883caaacd0c9f040a31870
khasakinil <khasakinil@gmail.com>
2023-10-22 16:23:05 +0530

    Booleans and operators operatins added

--- PythonBasicsExample/BooleansAndOperators.py
+++ PythonBasicsExample/BooleansAndOperators.py
@@ -3,5 +3,31 @@ like_coffee = True
 
 like_tea = False
 
+favorite_Food = "Kachori"
+
+favorite_number = 7
+
 print("Like Coffee : " , like_coffee)
 print("Like Tea : " , like_tea)
+print("_____________________________")
+print(type(favorite_Food))
+print(type(favorite_number))
+print(type(like_coffee))
+print(type(like_tea))
+
+print("Boolean Operators")
+print("_____________________________")
+print(3==6)
+print(8!=4)
+print(56>209)
+print(99<190)
+print(23>=23)
+print(65<=70)
+print("_____________________________")
+
+print("Logical Operators")
+print(34>45 and 50<70)
+print(34>45 or 50<70)
+print(not(4==4))
+
+print("_____________________________")
