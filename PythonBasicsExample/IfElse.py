3a5e9e79736f8ab06ddc9c30cbc9594ea8d73f8a
khasakinil <khasakinil@gmail.com>
2023-10-22 16:52:48 +0530

    IF Else statements

---
+++ PythonBasicsExample/IfElse.py
@@ -0,0 +1,29 @@
+
+x = 0
+
+if x > 1 :
+    print("x is greater than 1")
+else:
+    print("x is not greator than 1")
+
+print("outside of if statement")
+
+print("_____________________________________________")
+
+hour = 23
+if hour>=0 and hour<5 :
+    print("Midnight")
+elif hour <12 and hour >= 5:
+    print("good mornning")
+elif hour>=12 and hour<15:
+    print("good afternoon")
+elif hour>=15 and hour<=20 :
+    print("good evening")
+elif hour>20 and hour<=24 :
+    print("good night")
+else :
+    print("Invalid hours")
+
+print("_____________________________________________")
+
+
