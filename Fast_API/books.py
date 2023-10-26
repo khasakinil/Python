71bac59b618044d346c45aa000a7ddfc4c40de8a
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-10-26 06:47:57 +0530

    path parameters added

--- Fast_API/books.py
+++ Fast_API/books.py
@@ -14,3 +14,9 @@ BOOKS = [
 @app.get("/books/list")
 async def read_all_books():
     return BOOKS
+
+@app.get("/books/{book_title}")
+async def read_all_books(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
