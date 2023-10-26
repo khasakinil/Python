fa791477aca872ee2c84f1b8cccbc2cbab4c794c
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-10-26 06:24:46 +0530

    books added

--- Fast_API/books.py
+++ Fast_API/books.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/first-api")
-async def first_api():
-    return {"message" : "Hello, Good morning!!!"}
+BOOKS = [
+    {"title":"All Passion Spent", "Author":"Vita Sackville-West", "Category":"fiction novel"},
+    {"title":"Arms and the Man", "Author":"George Bernard Shaw", "Category":"Comedy"},
+    {"title":"Behold the Man", "Author":"Michael Moorcock", "Category":"science fiction"},
+    {"title":"A Little Learning", "Author":"Evelyn Waugh", "Category":"Autobiography"},
+    {"title":"Number the Stars", "Author":"Lois Lowry", "Category":"historical fiction"},
+    {"title":"The Waste Land", "Author":"T. S. Eliot", "Category":"poem"}
+]
+
+@app.get("/books/list")
+async def read_all_books():
+    return BOOKS
