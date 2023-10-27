3acb1fbc786828c5eea8dd9fd46fac93f3d81c38
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-10-27 10:10:28 +0530

    api with query params

--- Fast_API/books.py
+++ Fast_API/books.py
@@ -3,20 +3,32 @@ from fastapi import FastAPI
 app = FastAPI()
 
 BOOKS = [
-    {"title":"All Passion Spent", "Author":"Vita Sackville-West", "Category":"fiction novel"},
-    {"title":"Arms and the Man", "Author":"George Bernard Shaw", "Category":"Comedy"},
-    {"title":"Behold the Man", "Author":"Michael Moorcock", "Category":"science fiction"},
-    {"title":"A Little Learning", "Author":"Evelyn Waugh", "Category":"Autobiography"},
-    {"title":"Number the Stars", "Author":"Lois Lowry", "Category":"historical fiction"},
-    {"title":"The Waste Land", "Author":"T. S. Eliot", "Category":"poem"}
+    {"title":"All Passion Spent", "author":"Vita Sackville-West", "category":"historical fiction"},
+    {"title":"Arms and the Man", "author":"George Bernard Shaw", "category":"Comedy"},
+    {"title":"Behold the Man", "author":"Michael Moorcock", "category":"science fiction"},
+    {"title":"A Little Learning", "author":"Evelyn Waugh", "category":"Autobiography"},
+    {"title":"Number the Stars", "author":"Lois Lowry", "category":"historical fiction"},
+    {"title":"The Waste Land", "author":"T. S. Eliot", "category":"poem"}
 ]
 
+# api with endpoint
 @app.get("/books/list")
 async def read_all_books():
     return BOOKS
 
+# api with path paramter endpoint
 @app.get("/books/{book_title}")
 async def read_all_books(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
+
+# api with query parameter endpoint
+@app.get("/books/cat/")
+async def read_categoty_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
