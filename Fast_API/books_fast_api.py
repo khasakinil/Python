b286bf5b3303d49e3171f8a9386a333e05a79122
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-10-28 13:11:40 +0530

    request validation added
    request parameter validation added using pydantic library
    data validation using BaseModel and Field

--- Fast_API/books_fast_api.py
+++ Fast_API/books_fast_api.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
+
+#used for validation on data
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -16,6 +20,13 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id: Optional[int]
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=3)
+    description: str = Field(min_length=5, max_length=50)
+    rating: int = Field(gt=-1, lt=6)
+
 
 BOOKS = [Book(1, "All Passion Spent", "Vita Sackville-West", "category of historical fiction", 5),
          Book(2,"The Waste Land", "T. S. Eliot", "category of  Poem", 5), 
@@ -30,6 +41,13 @@ async def read_all_books():
     return BOOKS
 
 @app.post("/book/add")
-async def add_new_books(new_book=Body()):
-    BOOKS.append(new_book)
-    return BOOKS
+async def add_new_books(book_request: BookRequest):
+    new_book = Book(**book_request.dict())
+    BOOKS.append(find_book_by_id(new_book))
+    return new_book
+
+
+def find_book_by_id(book:Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
+
